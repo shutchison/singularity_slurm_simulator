83472fffc95227b01aba8e0f9d7e33902d87b39f
shutchison <scott.allen.hutchison@gmail.com>
2022-10-12 09:27:22 -0500

    troubleshooting container

--- run_folders/input_files/pre_simulation_script.sh
+++ run_folders/input_files/pre_simulation_script.sh
@@ -6,7 +6,7 @@
 
 echo "Doing pre simulation actions in pre_simulation_scipt.sh"
 # How to pass arguments into here???
-python3 /home/slurm/slurm_sim_ws/sim/micro/baseline/input_files/R_code_generator.py $*
+#python3 /home/slurm/slurm_sim_ws/sim/micro/baseline/input_files/R_code_generator.py $*
 
 # Use the R library to turn the trace file into a binary for injestion by the simulator
-R_LIBS_SITE=/home/slurm/R_packages Rscript /home/slurm/slurm_sim_ws/sim/micro/baseline/input_files/trace.R
+R_LIBS_SITE=/home/slurm/R_packages Rscript ${SIM_ROOT}/sim/micro/baseline/input_files/trace.R
---
+++ run_folders/input_files/trace.R
@@ -0,0 +1,62 @@
+library(RSlurmSimTools)
+
+trace <- list(
+    sim_job(
+        job_id = 1000,
+        submit = "2018-04-02 11:06:12",
+        wclimit = 1L,
+        duration = 3L,
+        tasks = 1L,
+        username = "user1",
+        account = "account1"
+    ),
+    sim_job(
+        job_id = 1001,
+        submit = "2018-04-02 11:19:12",
+        wclimit = 1L,
+        duration = 9L,
+        tasks = 1L,
+        username = "user1",
+        account = "account1"
+    ),
+    sim_job(
+        job_id = 1002,
+        submit = "2018-04-02 11:29:46",
+        wclimit = 304L,
+        duration = 18L,
+        tasks = 1L,
+        username = "user1",
+        account = "account1"
+    ),
+    sim_job(
+        job_id = 1003,
+        submit = "2018-04-02 11:46:14",
+        wclimit = 1L,
+        duration = 5L,
+        tasks = 1L,
+        username = "user1",
+        account = "account1"
+    ),
+    sim_job(
+        job_id = 1004,
+        submit = "2018-04-02 11:49:43",
+        wclimit = 42L,
+        duration = 8L,
+        tasks = 8L,
+        username = "user1",
+        account = "account1"
+    ),
+    sim_job(
+        job_id = 1005,
+        submit = "2018-04-02 12:00:49",
+        wclimit = 42L,
+        duration = 14L,
+        tasks = 8L,
+        username = "user1",
+        account = "account1"
+    ))
+
+
+trace <- do.call(rbind, lapply(trace,data.frame))
+
+write_trace("test.trace",trace)
